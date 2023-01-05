b38a3b0d22fd6b20fb848f960208b3924e0aa337
Igna <charreignacio@gmail.com>
2023-01-05 19:05:02 -0300

    Agregado nuevo archivo, explicación-conceptos.js

---
+++ explicacion-conceptos.js
@@ -0,0 +1,23 @@
+// ⚡Variables 
+
+// Las variables son como cajas donde puedes guardar información. Por ejemplo, si quieres recordar tu edad, puedes crear una variable llamada "edad" y asignarle tu edad como valor.
+
+// ⚡Strings
+
+// Las "strings" son cadenas de texto. Son un tipo de dato que se usa para representar texto en el código. Puedes usar comillas simples o comillas dobles para delimitar una cadena de texto.
+
+// ⚡Funciones 
+// las funciones en JavaScript son como máquinas que pueden hacer cosas por nosotros. Podemos escribir código en una función y decirle a la máquina que lo ejecute cuando necesitemos.
+// Las funciones siempre se invocan con la palabra "function" por delante. 
+
+ // ⚡Argumentos
+ // un argumento  es un valor que se le da a una función cuando se llama a la función.
+ // ⚡Return
+ // return se usa para indicar el resultado de una función
+
+// ⚡Declaración if
+// if se usa para ejecutar un trozo de código solo si se cumple cierta condición.
+
+//⚡Valores Booleanos
+// son valores que pueden ser "verdadero" (true) o "falso" (false). 
+//Los valores booleanos se usan a menudo en comparaciones para determinar si se cumple o no cierta condición.
